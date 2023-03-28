5892ac9842eb88377eb042615376d9cb0385b1fc
Mark <mark.szabo@nagarro.com>
2023-03-28 21:02:15 +0300

    s

--- src/test/java/ssvv/example/service/ServiceTest.java
+++ src/test/java/ssvv/example/service/ServiceTest.java
@@ -15,13 +15,11 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ServiceTest
-{
+class ServiceTest {
     private static Service service;
 
     @BeforeAll
-    static void setUp()
-    {
+    static void setUp() {
         Validator<Student> studentValidator = new StudentValidator();
 
         StudentXMLRepository studentRepository = new StudentXMLRepository(studentValidator, "test_student.xml");
@@ -32,23 +30,20 @@ class ServiceTest
     }
 
     @AfterEach
-    void tearDown()
-    {
-        try (FileWriter writer = new FileWriter("test_student.xml"))
-        {
-            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" + "<Entitati>\n" + "</Entitati>\n");
-        } catch (IOException e)
-        {
+    void tearDown() {
+        try (FileWriter writer = new FileWriter("test_student.xml")) {
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" + "<Entitati>\n" +
+                    "</Entitati>\n");
+        } catch (IOException e) {
         }
     }
 
     @Test
-    void saveStudent_shouldReturn1WhenTheInputIsValid()
-    {
+    void saveStudent_shouldReturn1WhenTheInputIsValid() {
         // given
-        String id = "1";
-        String nume = "Uga";
-        int grupa = 222;
+        String id = "id";
+        String nume = "name";
+        int grupa = 120;
 
         // when
         int result = service.saveStudent(id, nume, grupa);
@@ -58,17 +53,170 @@ class ServiceTest
     }
 
     @Test
-    void saveStudent_shouldReturn0WhenTheNameIsEmpty()
-    {
+    void saveStudent_shouldReturn0WhenGroupLessThanBoundary() {
         // given
-        String id = "1";
-        String nume = "";
-        int grupa = 222;
+        String id = "id";
+        String nume = "name";
+        int grupa = 110;
 
         // when
         int result = service.saveStudent(id, nume, grupa);
 
         // then
         assertEquals(0, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn0WhenGroupGreaterThanBoundary() {
+        // given
+        String id = "id";
+        String nume = "name";
+        int grupa = 938;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn0WhenIdNull() {
+        // given
+        String id = null;
+        String nume = "name";
+        int grupa = 120;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn0WhenIdBlank() {
+        // given
+        String id = "";
+        String nume = "name";
+        int grupa = 120;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn0WhenNameNull() {
+        // given
+        String id = "id";
+        String nume = null;
+        int grupa = 120;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn0WhenNameBlank() {
+        // given
+        String id = "id";
+        String nume = "";
+        int grupa = 120;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn1WhenGroupIsLowerBoundary() {
+        // given
+        String id = "id";
+        String nume = "name";
+        int grupa = 111;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn1WhenGroupIsHigherBoundary() {
+        // given
+        String id = "id";
+        String nume = "name";
+        int grupa = 937;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn1WhenGroupIsHigherBoundaryNeighbour() {
+        // given
+        String id = "id";
+        String nume = "name";
+        int grupa = 936;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn1WhenGroupIsLowerBoundaryNeighbour() {
+        // given
+        String id = "id";
+        String nume = "name";
+        int grupa = 112;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn1WhenLenName1() {
+        // given
+        String id = "id";
+        String nume = "n";
+        int grupa = 937;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    void saveStudent_shouldReturn1WhenLenId1() {
+        // given
+        String id = "i";
+        String nume = "name";
+        int grupa = 937;
+
+        // when
+        int result = service.saveStudent(id, nume, grupa);
+
+        // then
+        assertEquals(1, result);
     }
 }
