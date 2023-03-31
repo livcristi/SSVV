12926a4217bac25923a3c9ccf1bdee87a2bad85f
Mark <mark.szabo@nagarro.com>
2023-03-31 19:07:29 +0300

    2 tests

---
+++ src/test/java/ssvv/example/service/WhiteBoxServiceTest.java
@@ -0,0 +1,62 @@
+package ssvv.example.service;
+
+import jdk.jfr.ValueDescriptor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import ssvv.example.domain.Student;
+import ssvv.example.domain.Tema;
+import ssvv.example.repository.NotaXMLRepository;
+import ssvv.example.repository.StudentXMLRepository;
+import ssvv.example.repository.TemaXMLRepository;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
+import ssvv.example.validation.Validator;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WhiteBoxServiceTest {
+
+    private static Service service;
+
+    @BeforeAll
+    static void setUp() {
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Student> studentValidator = new StudentValidator();
+
+        StudentXMLRepository studentRepository = null;
+        TemaXMLRepository temaRepository = new TemaXMLRepository(temaValidator, "test_tema.xml");
+        NotaXMLRepository notaRepository = null;
+
+        service = new Service(studentRepository, temaRepository, notaRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        try (FileWriter writer = new FileWriter("test_tema.xml")) {
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" + "<Entitati>\n" +
+                    "</Entitati>\n");
+        } catch (IOException e) {
+        }
+    }
+
+    @Test
+    void saveTema_shouldReturn0WhenIdNull(){
+        int actualResult = service.saveTema(null, "descriere", 1, 1);
+
+        assertEquals(0,actualResult);
+    }
+
+    @Test
+    void saveTema_shouldReturn0WhenDescriereNull(){
+        int actualResult = service.saveTema("1", null, 1, 1);
+
+        assertEquals(0,actualResult);
+    }
+
+
+
+}
